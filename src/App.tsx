b207b1d5dcd8ccf7b688933d3e64d26ced6eade8
Alina <palinchuk2016@gmail.com>
2026-03-09 21:36:18 +0200

    setup react structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="min-h-screen">
+            <main className="max-w-7xl mx-auto px-4 py-6 flex justify-center">
+                main koren marketplace
+            </main>
+        </div>
+    </>
+  )
+}
+
+export default App
